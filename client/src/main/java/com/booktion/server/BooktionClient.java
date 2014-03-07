b0d91822ee85e965037beb06aa517d064aebf081
Bence Kiglics <bence.kiglics@gmail.com>
2014-03-07 12:43:03 +0100

    finished server-client comminucation

--- client/src/main/java/com/booktion/server/BooktionClient.java
+++ client/src/main/java/com/booktion/server/BooktionClient.java
@@ -1,10 +1,28 @@
 package com.booktion.server;
 
+import com.booktion.thrift.BooktionService;
+import com.booktion.thrift.Message;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+
 public class BooktionClient
 {
     public void run()
     {
         System.out.println("Booktion client is starting up...");
+
+        try {
+            TTransport transport = new TSocket("localhost", 1234);
+            transport.open();
+
+            TProtocol protocol = new TBinaryProtocol(transport);
+            BooktionService.Client client = new BooktionService.Client(protocol);
+            client.echo(new Message("I am Client!"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args)
--- server/src/main/java/com/booktion/server/BooktionHandler.java
+++ server/src/main/java/com/booktion/server/BooktionHandler.java
@@ -4,11 +4,28 @@ import com.booktion.thrift.BooktionService;
 import com.booktion.thrift.Message;
 import org.apache.thrift.TException;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class BooktionHandler implements BooktionService.Iface
 {
     @Override
     public void echo(Message message) throws TException
     {
-        System.out.println(message.getText());
+        System.out.println(String.format("%s %s", getTimestampString(), message.getText()));
+    }
+
+    private String getTimestampString() {
+        Calendar calendar = new GregorianCalendar();
+
+        return String.format(
+                "[%04d-%02d-%02d %02d:%02d:%02d]",
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH),
+                calendar.get(Calendar.HOUR_OF_DAY),
+                calendar.get(Calendar.MINUTE),
+                calendar.get(Calendar.SECOND)
+        );
     }
 }
