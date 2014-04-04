73b8a827b0d1f1cf7f9dea0d212590c9b78c07b5
Bence Kiglics <bence.kiglics@gmail.com>
2014-04-04 07:33:23 +0200

    finished tests for BooktionConnector

--- client/src/main/java/com/booktion/client/connector/BooktionConnectorFactory.java
+++ client/src/main/java/com/booktion/client/connector/BooktionConnectorFactory.java
@@ -5,7 +5,7 @@ public class BooktionConnectorFactory
     public static BooktionConnector create()
     {
         BooktionConnector connector = new BooktionConnector();
-        connector.setProtocolFactory(new ProtocolFactory());
+        connector.setProtocolFactory(new ClientFactory());
 
         return connector;
     }
