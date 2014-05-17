54b5ddfb068aafe37228a89c58ff66263c53d3cc
Bence Kiglics <bence.kiglics@gmail.com>
2014-05-17 19:46:35 +0200

    set native look and feel on every platform

--- client/src/main/java/com/booktion/client/BooktionClient.java
+++ client/src/main/java/com/booktion/client/BooktionClient.java
@@ -3,14 +3,33 @@ package com.booktion.client;
 import com.booktion.client.controller.MainController;
 import com.booktion.client.gui.JMainWindow;
 
+import javax.swing.*;
+
 public class BooktionClient
 {
     public void run()
     {
+        setLookAndFeel();
+
         JMainWindow window = new JMainWindow();
         window.setVisible(true);
 
         new MainController(window);
+    }
+
+    private void setLookAndFeel()
+    {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args)
