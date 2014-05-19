0eacef48f5a2db7ae9871d24c17c84aa7af9e220
Bence Kiglics <bence.kiglics@gmail.com>
2014-05-19 20:40:11 +0200

    turned back system look and feel (removed dependency for substance too)

--- client/src/main/java/com/booktion/client/BooktionClient.java
+++ client/src/main/java/com/booktion/client/BooktionClient.java
@@ -2,10 +2,6 @@ package com.booktion.client;
 
 import com.booktion.client.controller.MainController;
 import com.booktion.client.gui.JMainWindow;
-import org.pushingpixels.substance.api.skin.SubstanceBusinessLookAndFeel;
-import org.pushingpixels.substance.api.skin.SubstanceCeruleanLookAndFeel;
-import org.pushingpixels.substance.api.skin.SubstanceDustLookAndFeel;
-import org.pushingpixels.substance.api.skin.SubstanceGraphiteLookAndFeel;
 
 import javax.swing.*;
 
@@ -31,8 +27,14 @@ public class BooktionClient
     private void setLookAndFeel()
     {
         try {
-            UIManager.setLookAndFeel(new SubstanceGraphiteLookAndFeel());
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
 
