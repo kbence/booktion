a71cce63c26727e8c139252186ee08595bf44536
Bence Kiglics <bence.kiglics@gmail.com>
2014-05-21 20:04:38 +0200

    listing user's book on server side

--- server/src/main/java/com/booktion/server/db/BookDAO.java
+++ server/src/main/java/com/booktion/server/db/BookDAO.java
@@ -2,7 +2,10 @@ package com.booktion.server.db;
 
 import com.booktion.server.model.Book;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,8 +14,9 @@ public class BookDAO extends DAO
     private final String INSERT_BOOK = "INSERT INTO books (title, owner, author, publisher, " +
             "yearOfPublication, condition) VALUES (?, ?, ?, ?, ?, ?)";
     private final String SELECT_BOOK_BY_ID = "SELECT * FROM books WHERE id = ?";
+    private final String SELECT_BOOK_BY_OWNER = "SELECT * FROM books WHERE owner = ?";
 
-        public BookDAO(Connection connection)
+    public BookDAO(Connection connection)
     {
         super(connection);
     }
@@ -50,20 +54,44 @@ public class BookDAO extends DAO
             ResultSet result = stmt.executeQuery();
 
             if (result.next()) {
-                Book book = new Book();
-                book.id = result.getInt("id");
-                book.owner = result.getInt("owner");
-                book.title = result.getString("title");
-                book.author = result.getString("author");
-                book.publisher = result.getString("publisher");
-                book.yearOfPublication = result.getShort("yearOfPublication");
-                book.condition = result.getShort("condition");
-                return book;
+                return createBookFromResult(result);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
         return null;
+    }
+
+    public List<Book> getByOwner(int ownerId)
+    {
+        ArrayList<Book> books = new ArrayList<Book>();
+
+        try {
+            PreparedStatement stmt = connection.prepareStatement(SELECT_BOOK_BY_OWNER);
+            stmt.setInt(1, ownerId);
+            ResultSet result = stmt.executeQuery();
+
+            while (result.next()) {
+                books.add(createBookFromResult(result));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return books;
+    }
+
+    private Book createBookFromResult(ResultSet result) throws SQLException
+    {
+        Book book = new Book();
+        book.id = result.getInt("id");
+        book.owner = result.getInt("owner");
+        book.title = result.getString("title");
+        book.author = result.getString("author");
+        book.publisher = result.getString("publisher");
+        book.yearOfPublication = result.getShort("yearOfPublication");
+        book.condition = result.getShort("condition");
+        return book;
     }
 }
